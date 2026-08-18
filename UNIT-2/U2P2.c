8878a050f117e00a3976a7467c5c84f363019dfb
DIGVIJAYSINH <digvijaysinhh33@gmail.com>
2026-08-18 18:43:56 +0530

    Add files via upload

---
+++ UNIT-2/U2P2.c
@@ -0,0 +1,24 @@
+/* Write a program to find out the factorial of a number using recursion(stack).
+Name: DIGVIJAYSINH SODHAPARMAR
+Enrollment no.:92500527123 */
+
+#include<conio.h>
+int factorial(int n);
+void main()
+{
+    int n,z;
+    printf("Enter value for factorial:");
+    scanf("%d",&n);
+    z=factorial(n);
+    printf("Factorial of %d is %d.",n,z);
+}
+
+int factorial(int n)
+{
+    int f;
+    if(n==1)
+        return 1;
+    else
+        f=n*factorial(n-1);
+    return f;
+}
---
+++ UNIT-2/U2P8_2.c
@@ -0,0 +1,44 @@
+/* Write a program to find Minimum and Maximum numbers from the given array with recursion.
+Name: DIGVIJAYSINH SODHAPARMAR
+Enrollment no.:92500527123 */
+
+#include<stdio.h>
+void findMinMax(int arr[],int n,int index,int *min,int *max);
+void main()
+{
+    int arr[100],n,i;
+    int min,max;
+
+    printf("Enter size of array :");
+    scanf("%d",&n);
+
+    printf("Enter %d elements :",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    max=min=arr[0];
+
+    findMinMax(arr,n,1,&min,&max);
+    printf("Maximum = %d\n",max);
+    printf("Minimum = %d\n",min);
+}
+
+void findMinMax(int arr[],int n,int index,int *min,int *max)
+{
+    if(index==n)
+    {
+        return;
+    }
+    if(arr[index]<*min)
+    {
+        *min=arr[index];
+    }
+    if(arr[index]>*max)
+    {
+        *max=arr[index];
+    }
+
+    findMinMax(arr,n,index+1,min,max);
+}
